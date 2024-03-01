dd2e71bd5ac99f267560deb751664fba8c369071
zhens297 <zhens297@newschool.edu>
2024-02-29 19:33:43 -0500

    skip and back

--- p1/uninteresting/script.js
+++ p1/uninteresting/script.js
@@ -4,14 +4,31 @@ document.addEventListener('DOMContentLoaded', function() {
     textElement.innerText = '';
 
     let i = 0;
+    let typingSpeed = 30; // typing speed
+    let typingTimeout; 
+
     function typeWriter() {
         if (i < textContent.length) {
             textElement.innerHTML += textContent.charAt(i);
             i++;
-            setTimeout(typeWriter, 20); // Adjust typing speed here
+            typingTimeout = setTimeout(typeWriter, typingSpeed);
         }
+    }
+
+    function skipTyping() {
+        clearTimeout(typingTimeout); 
+        textElement.innerText = textContent; 
+       
+        console.log('Typewriter animation skipped!');
+
+        const uninterestingElement = document.querySelector('.uninteresting');
+    uninterestingElement.style.animationDelay = '1.2s';
     }
 
     // Start the typewriter effect when the page loads
     typeWriter();
+
+    const skipButton = document.querySelector('.skip');
+    skipButton.addEventListener('click', skipTyping);
+    
 });
