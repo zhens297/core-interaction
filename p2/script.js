5fa8934872c4c1c099b09686d9316d4f1a6eeb92
zhens297 <zhens297@newschool.edu>
2024-04-04 22:18:55 -0400

    data

--- p2/script.js
+++ p2/script.js
@@ -8,37 +8,100 @@ const objects =
             // "completed": true
         },
         {
-            "name": "Learn CSS",
+            "name": "asics",
             "img": 'asics.png',
             "type": "shoe",
             // "time": 5,
             // "completed": true
         },
         {
-            "name": "Learn Javascript",
+            "name": "bowl",
             "img": 'bowl.png',
             "type": "ceramic",
             // "time": 10,
             // "completed": true
         },
         {
-            "name": "Learn Figma",
+            "name": "noguchi",
             "img": 'noguchi.png',
             "type": "home",
             // "time": 20,
             // "completed": false
         },
         {
-            "name": "Learn p5",
+            "name": "moon",
             "img": 'moon.png',
             "type": "home",
             // "time": 15,
             // "completed": false
         },
         {
-            "name": "my dream dog",
+            "name": "teapot",
             "img": 'teapot.png',
             "type": "ceramic",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "heel",
+            "img": 'heel.png',
+            "type": "shoe",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "knot",
+            "img": 'knot.png',
+            "type": "accessory",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "necklace",
+            "img": 'necklace.png',
+            "type": "accessory",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "pampshade",
+            "img": 'pampshade.png',
+            "type": "home",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "teapot",
+            "img": 'lamp.jpg',
+            "type": "music",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "teapot",
+            "img": 'sony.png',
+            "type": "music",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "teapot",
+            "img": 'stool.png',
+            "type": "home",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "teapot",
+            "img": 'tuk.png',
+            "type": "shoe",
+            // "time": -20,
+            // "completed": false
+        },
+        {
+            "name": "teapot",
+            "img": 'tulip.png',
+            "type": "accessory",
             // "time": -20,
             // "completed": false
         }
@@ -63,28 +126,7 @@ function renderContent() {
         const object = objects[i];
 
         // if (object.time >= minTime) {
-        if (showCategory === 'all') {
-
-            const objectElement = document.createElement('div');
-
-            objectElement.className = 'object';
-
-            // if (object.type === showCategory) {
-            //     objectElement.className += ' accessory';
-            // }
-
-            // objectElement.addEventListener('click', function () {
-            //     objectElement.classList.toggle('completed');
-            // });
-
-            const img = document.createElement('img');
-            img.src = 'img/' + object.img;
-            img.alt = object.name;
-
-            // objectElement.innerHTML = `<p>${object.name} - ${object.time} hours</p>`
-            objectElement.appendChild(img);
-            toolbox.appendChild(objectElement);
-        } else if (object.type === showCategory) {
+        if (showCategory === 'all' || object.type === showCategory) {
 
             const objectElement = document.createElement('div');
 
@@ -106,7 +148,6 @@ function renderContent() {
             objectElement.appendChild(img);
             toolbox.appendChild(objectElement);
         }
-        // }
     }
 }
 
